5cf92ce0a2a30e33d433f60b9d95c50a742c75ff
Matthew Carter <m@ahungry.com>
2022-09-22 21:44:23 -0400

    Some working output and encoding

--- src/ahungry/overdexer/resource/itm.clj
+++ src/ahungry/overdexer/resource/itm.clj
@@ -1,11 +1,32 @@
 (ns ahungry.overdexer.resource.itm
   (:require
-   [gloss.core :as gc]
-   [gloss.io :as gi]))
+   [clojure.java.io]
+   [gloss.core :as c]
+   [gloss.io :as io]))
+
+;; https://gibberlings3.github.io/iesdp/file_formats/ie_formats/itm_v1.htm
+
+;; https://github.com/clj-commons/gloss/wiki/Introduction
+;; https://cljdoc.org/d/org.clj-commons/gloss/0.3.0/api/gloss
+;; https://github.com/d-t-w/by-example-gloss/blob/master/src/by_example_gloss/core.clj
+
+(def sig-frame (c/compile-frame {:name (c/string :utf-8 :length 4)}))
 
 (defn get-item [s]
   (slurp (str "/home/mcarter/bgee/bgee2/override/" s)))
 
 (defn foo []
   (prn "Yay")
-  gi/decode)
+  io/decode)
+
+(defn d []
+  (io/decode sig-frame (get-item "qdmfist.itm")))
+
+(defn e []
+  (io/encode sig-frame {:name "matt"}))
+
+(defn out-to-file []
+  (let [records [{:name "matt"}]]
+    (with-open
+      [out-stream (clojure.java.io/output-stream "file:///tmp/x.log")]
+      (io/encode-to-stream sig-frame out-stream records))))
