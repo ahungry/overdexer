390f7d80b82aab387cd9525e6b9213b0276fc327
Matthew Carter <m@ahungry.com>
2022-09-23 00:24:02 -0400

    Add note

--- src/ahungry/overdexer/resource/itm.clj
+++ src/ahungry/overdexer/resource/itm.clj
@@ -10,19 +10,21 @@
 ;; https://cljdoc.org/d/org.clj-commons/gloss/0.3.0/api/gloss
 ;; https://github.com/d-t-w/by-example-gloss/blob/master/src/by_example_gloss/core.clj
 
+;; NOTE: strref = int reference in dialog.tlk, resref = 8 byte ascii w/ garbage
+;; char array = X byte ascii, everything else = little endian values
 (def header-frame
   (c/ordered-map
    :signature (c/string :utf-8 :length 4)
    :version (c/string :utf-8 :length 4)
-   :unidentified-name :int32
-   :identified-name :int32
-   :replacement-item :int64
-   :flags :int32
-   :item-type :int16
-   :usability-bitmask :int32
-   :item-animation :int16
-   :min-level :int16
-   :min-strength :int16
+   :unidentified-name :int32-le
+   :identified-name :int32-le
+   :replacement-item (c/string :utf-8 :length 8)
+   :flags :int32-le
+   :item-type :int16-le
+   :usability-bitmask :int32-le
+   :item-animation (c/string :utf-8 :length 2) ;; :int16-le
+   :min-level :int16-le
+   :min-strength :int16-le
    :min-strength-bonus :byte
    :kit-usability-1 :byte
    :min-intelligence :byte
@@ -33,32 +35,33 @@
    :kit-usability-4 :byte
    :min-constitution :byte
    :weapon-proficiency :byte
-   :min-charisma :int16
-   :price :int32
-   :stack-amount :int16
-   :inventory-icon :int64
-   :lore-to-id :int16
-   :ground-icon :int64
-   :weight :int32
-   :unidentified-description :int32
-   :identified-description :int32
-   :description-icon :int64
-   :enchantment :int32
-   :offset-to-extended-headers :int32
-   :count-of-extended-headers :int16
-   :offset-to-feature-blocks :int32
-   :index-into-feature-blocks :int16
-   :count-of-feature-blocks :int16
-   ))
+   :min-charisma :int16-le
+   :price :int32-le
+   :stack-amount :int16-le
+   :inventory-icon (c/string :utf-8 :length 8)
+   :lore-to-id :int16-le
+   :ground-icon (c/string :utf-8 :length 8)
+   :weight :int32-le
+   :unidentified-description :int32-le
+   :identified-description :int32-le
+   :description-icon (c/string :utf-8 :length 8)
+   :enchantment :int32-le
+   :offset-to-extended-headers :int32-le
+   :count-of-extended-headers :int16-le
+   :offset-to-feature-blocks :int32-le
+   :index-into-feature-blocks :int16-le
+   :count-of-feature-blocks :int16-le))
 
 ;; FIXME: Probably read directly into the java.nio.HeapByteBuffer this creates,
 ;; instead of slurping and then translating
 (defn get-item [s]
   (let [content (slurp (str "/home/mcarter/bgee/bgee2/override/" s))]
-    (java.nio.ByteBuffer/wrap (.getBytes content java.nio.charset.StandardCharsets/US_ASCII))))
+    (->
+     (java.nio.ByteBuffer/wrap (.getBytes content java.nio.charset.StandardCharsets/US_ASCII))
+     (.order java.nio.ByteOrder/LITTLE_ENDIAN))))
 
-(defn test-item-header []
-  (io/decode header-frame (.slice (get-item "qdmfist.itm") 0 0x72)))
+(defn test-item-header [s]
+  (io/decode header-frame (.slice (get-item s) 0 0x72)))
 
 (defn foo []
   (prn "Yay"))
