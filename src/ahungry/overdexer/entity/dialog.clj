a18d8b782e9247bb5bea6e3fa51c0f7024ca3799
Matthew Carter <m@ahungry.com>
2022-09-24 21:32:57 -0400

    Working parse of dialog into entries

--- src/ahungry/overdexer/entity/dialog.clj
+++ src/ahungry/overdexer/entity/dialog.clj
@@ -29,3 +29,39 @@
    :offset          (ie/dword  4)
    :length          (ie/dword  4)
    ])
+
+(def string-spec
+  (c/repeated
+   :byte
+   :prefix (c/prefix entry-spec #(get % :length) str)))
+
+(def header-frame (apply c/ordered-map header-spec))
+(def entry-frame (apply c/ordered-map entry-spec))
+
+(defn get-dialog []
+  (with-open
+    [in-stream (clojure.java.io/input-stream
+                "/home/mcarter/bgee/bgee2/lang/en_US/dialog.tlk")]
+    (->
+     (java.nio.ByteBuffer/wrap (.readAllBytes in-stream))
+     (.order java.nio.ByteOrder/LITTLE_ENDIAN))))
+
+(defn parse-dialog []
+  (let [dialog (get-dialog)
+        header (io/decode header-frame (.slice dialog 0 18))]
+    {:header header
+
+     :entries
+     (map (fn [i]
+            (let [entry (io/decode entry-frame (.slice dialog (+ 18 (* i 26)) 26))]
+              {:entry entry
+               :string (io/decode
+                        (c/string :utf-8 :length (:length entry))
+                        (.slice dialog
+                                (+ (:offset-to-string-data header)
+                                   (:offset entry))
+                                (:length entry)
+                                ))}))
+          (range (:number-of-strref-entries header)))
+     }
+    ))
