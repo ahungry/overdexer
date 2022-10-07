05e171e3e22f45e186720fb0b51ad16e6ee86a98
Matthew Carter <m@ahungry.com>
2022-10-07 19:16:57 -0400

    Give a version style endpoint

--- src/ahungry/overdexer/server.clj
+++ src/ahungry/overdexer/server.clj
@@ -1,8 +1,34 @@
 (ns ahungry.overdexer.server
   (:require
+   [clojure.data.json :as json]
    [clojure.pprint]
    [ring.adapter.jetty :as jetty]
+   [compojure.core :as compojure]
+   [compojure.route :as compojure-route]
    ))
+
+(compojure/defroutes my-routes
+  (compojure/GET "/" [] "Hello World")
+  (compojure/GET "/version" [] {:body {:version "0.0.1"}})
+  (compojure-route/not-found "Page not found"))
+
+(defn wrap-headers [handler]
+  (fn [req]
+    (prn "wrap-headers")
+    (let [res (handler req)]
+      (-> res (assoc-in [:headers "content-type"] "application/json")))))
+
+(defn wrap-json [handler]
+  (fn [req]
+    (prn "wrap-json")
+    (let [res (handler req)]
+      (-> res (update-in [:body] json/write-str)))))
+
+(def app
+  (compojure/routes
+   (-> my-routes
+       (compojure/wrap-routes #'wrap-headers)
+       (compojure/wrap-routes #'wrap-json))))
 
 (defn handler [request]
   (clojure.pprint/pprint request)
@@ -13,6 +39,6 @@
 (defn start
   [& args]
   (jetty/run-jetty
-   handler
+   app
    {:port 3000
     :join? true}))
